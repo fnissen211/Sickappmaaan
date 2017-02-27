0c4d176e5909e8dcb73bdf4a46e8dabd22a29e97
fnissen211 <frederikbnissen@gmail.com>
2017-02-27 21:21:12 +0100

    Nyt View

---
+++ SickAppMaaan/NewViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NewViewController.swift
+//  SickAppMaaan
+//
+//  Created by Frederik Nissen on 27/02/2017.
+//  Copyright © 2017 Nissen Studio. All rights reserved.
+//
+
+import UIKit
+
+class NewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
