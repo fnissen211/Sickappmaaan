220b33821f493a3570ce8fe4891d6e79022855d2
fnissen211 <frederikbnissen@gmail.com>
2017-02-27 20:37:40 +0100

    First Try

--- SickAppMaaan/StartViewController.swift
+++ SickAppMaaan/StartViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  StartViewController.swift
 //  SickAppMaaan
 //
 //  Created by Frederik Nissen on 27/02/2017.
@@ -8,18 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StartViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+
 
 
 }
-
